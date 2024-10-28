da4116f62773f9fb1f47e288684dfc8f75efaec3
Pedro Rocha <68920103+pedrolucasdev@users.noreply.github.com>
2024-10-28 11:34:26 +0000

    Added custom cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import CallToAction from "./components/CallToAction";
 import CaseStudies from "./components/CaseStudies";
+import CustomCursor from "./components/common/CustomCursor";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -11,7 +12,8 @@ import WorkingProcess from "./components/WorkingProcess";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-full pt-[30px] md:pt-10 items-center ">
+    <div className="relative flex flex-col w-full h-full pt-[30px] md:pt-10 items-center ">
+      <CustomCursor></CustomCursor>
       <div className="flex flex-col max-w-[1340px] w-full">
         <Header></Header>
         <Hero></Hero>
